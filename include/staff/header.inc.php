a830d01be8e140349e844dbeb2dc1cadb7ae75e7
Luke Arms <luke@linacreative.com>
2018-08-04 15:16:09 +1000

    Syntax fix.

--- include/staff/header.inc.php
+++ include/staff/header.inc.php
@@ -34,11 +34,11 @@ if ($lang) {
     <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/redactor.css" media="screen">
     <link rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/typeahead.css" media="screen">
     <link type="text/css" href="<?php echo ROOT_PATH; ?>css/ui-lightness/jquery-ui-1.10.3.custom.min.css"
+         rel="stylesheet" media="screen" />
 <link rel="icon" href="<?php echo ROOT_PATH; ?>cropped-LINA-creative-target-colour-200x200.png" sizes="32x32" />
 <link rel="icon" href="<?php echo ROOT_PATH; ?>cropped-LINA-creative-target-colour-200x200.png" sizes="192x192" />
 <link rel="apple-touch-icon-precomposed" href="<?php echo ROOT_PATH; ?>cropped-LINA-creative-target-colour-200x200.png" />
 <meta name="msapplication-TileImage" content="<?php echo ROOT_PATH; ?>cropped-LINA-creative-target-colour-300x300.png" />
-         rel="stylesheet" media="screen" />
      <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/font-awesome.min.css">
     <!--[if IE 7]>
     <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/font-awesome-ie7.min.css">
